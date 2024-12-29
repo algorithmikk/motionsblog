932c33d7109b520b77a060214ea3e30682696784
jay <jawadarifpro@gmail.com>
2024-12-29 12:25:30 +0100

    add new project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'Restaurant Management Software',
+    description: `Building a complete cloud based software solution for restaurant/private cooks to manager orders, billing, menu and payments`,
+    imgSrc: '/static/images/umameats.png',
+    href: 'https://umameats.com',
+  },
+  {
     title: 'Real Time payment systems',
     description: `Modernizing payment systems for major Canadian banks in the cloud`,
     imgSrc: '/static/images/cloud.png',
@@ -17,7 +23,8 @@ const projectsData: Project[] = [
     description: `Implementing DevOps principles using secure software principles`,
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/dev-sec-ops',
-  },
+  }
+
 ]
 
 export default projectsData
